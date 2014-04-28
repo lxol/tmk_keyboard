21ee2c0b134b4a6d6c9bff09338274a43f02e2ed
Alex Olkhovskiy <alex@olkhovskiy.com>
2014-04-28 10:32:20 +0100

    Rebind RAlt -> RGUI

--- keyboard/hhkb/keymap_alexol.c
+++ keyboard/hhkb/keymap_alexol.c
@@ -19,14 +19,14 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |-----------------------------------------------------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .| Fn2|Shift |Fn0|
      * `-----------------------------------------------------------'
-     *       |Gui|Alt  |         Space         |LAlt  |Control|
+     *       |Gui|Alt  |         Space         |LAlt  |GUI|
      *       `-----------------------------------------------'
      */
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
            LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN3,QUOT,FN1,             \
            LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, FN2,RSFT,FN0,             \
-                LGUI,LALT,          SPC,                LALT,RCTL),
+                LGUI,LALT,          SPC,                LALT,RGUI),
 
     /* Layer 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
@@ -68,7 +68,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 
     /* Layer 3: Mouse mode (Semicolon)
      * ,-----------------------------------------------------------.
-     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
+     * |Esc| F1| F2|  F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
      * |-----------------------------------------------------------|
      * |Tab  |   |   |   |   |   |MwL|MwD|MwU|MwR|   |   |   |Backs|
      * |-----------------------------------------------------------|
