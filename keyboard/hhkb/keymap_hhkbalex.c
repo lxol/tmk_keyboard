2897ad3df11436cd5167f2167d2eac87d8feb141
Alex Olkhovskiy <alex@olkhovskiy.com>
2016-01-03 11:55:32 +0000

    wip

--- keyboard/hhkb/keymap_hhkbalex.c
+++ keyboard/hhkb/keymap_hhkbalex.c
@@ -3,11 +3,16 @@
  */
 #include "keymap_common.h"
 
+#define BASE 0 // default layer
+#define HHKB 1 // symbols
+#define VI 2 // media keys
+#define MOUSE 3 // unicode layer
+
 
 #ifdef KEYMAP_SECTION_ENABLE
-const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] __attribute__ ((section (".keymap.keymaps"))) = {
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] __attribute__ ((section (".keymap.keymaps"))) = {
 #else
-const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
     /* Layer 0: Default Layer
      * ,-----------------------------------------------------------.
@@ -22,11 +27,11 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       |Gui|Alt  |         Space         |LAlt  |Control|
      *       `-----------------------------------------------'
      */
-    KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
-           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
-           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN3,QUOT,FN1,             \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, FN2,RSFT,FN0,             \
-                LGUI,LALT,          SPC,                LALT,RCTL),
+[BASE] =   KEYMAP(KC_ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV,   \
+           KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSPC,       \
+           KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   RGUI(KC_J),   KC_K,   KC_L,   KC_FN3, KC_QUOT, KC_FN1,             \
+           KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_FN2, KC_RSFT,KC_FN0,             \
+                KC_LGUI, KC_LALT,          KC_SPC,                KC_LALT, KC_RCTL),
 
     /* Layer 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
@@ -41,11 +46,11 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
-    KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   \
-           CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, BSPC,      \
-           TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,            \
-           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,            \
-                TRNS,TRNS,          TRNS,               TRNS,TRNS),
+[HHKB] =   KEYMAP(KC_PWR, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,   \
+           KC_CAPS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, KC_TRNS, KC_BSPC,      \
+           KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE,KC_TRNS,KC_TRNS, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_PENT,            \
+           KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, KC_TRNS,KC_TRNS,            \
+                KC_TRNS,KC_TRNS,          KC_TRNS,               KC_TRNS,KC_TRNS),
 
     /* Layer 2: Vi mode (Slash)
      * ,-----------------------------------------------------------.
@@ -60,11 +65,11 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       |Gui|Alt  |          Space        |Alt  |Gui|
      *       `-------------------------------------------'
      */
-    KEYMAP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
-           TAB, HOME,PGDN,UP,  PGUP,END, HOME,PGDN,PGUP,END, NO,  NO,  NO,  BSPC, \
-           LCTL,NO,  LEFT,DOWN,RGHT,NO,  LEFT,DOWN,UP,  RGHT,NO,  NO,  ENT, \
-           LSFT,NO,  NO,  NO,  NO,  NO,  HOME,PGDN,PGUP,END, TRNS,RSFT,NO, \
-                LGUI,LALT,          SPC,                RALT,RGUI),
+[VI] =    KEYMAP( KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL, \
+           KC_TAB, KC_HOME, KC_PGDN, KC_UP,  KC_PGUP, KC_END, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO,  KC_NO,  KC_NO,  KC_BSPC, \
+           KC_LCTL, KC_NO,  KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,  KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_NO,  KC_NO,  KC_ENT, \
+           KC_LSFT, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TRNS, KC_RSFT, KC_NO, \
+                KC_LGUI, KC_LALT,          KC_SPC,                KC_RALT, KC_RGUI),
 
     /* Layer 3: Mouse mode (Semicolon)
      * ,-----------------------------------------------------------.
@@ -80,11 +85,11 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *      `--------------------------------------------'
      * Mc: Mouse Cursor / Mb: Mouse Button / Mw: Mouse Wheel
      */
-    KEYMAP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
-           TAB, NO,  NO,  NO,  NO,  NO,  WH_L,WH_D,WH_U,WH_R,NO,  NO,  NO,  BSPC, \
-           LCTL,NO,  ACL0,ACL1,ACL2,NO,  MS_L,MS_D,MS_U,MS_R,TRNS,QUOT,ENT, \
-           LSFT,NO,  NO,  NO,  NO,  BTN3,BTN2,BTN1,BTN4,BTN5,SLSH,RSFT,NO, \
-                LGUI,LALT,          BTN1,               RALT,TRNS),
+ [MOUSE] = KEYMAP( KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL, \
+           KC_TAB, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO,  KC_NO,  KC_NO,  KC_BSPC, \
+           KC_LCTL, KC_NO,  KC_ACL0, KC_ACL1, KC_ACL2, KC_NO,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,KC_TRNS, KC_QUOT, KC_ENT, \
+           KC_LSFT, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_BTN3, KC_BTN2, KC_BTN1, KC_BTN4, KC_BTN5, KC_SLSH, KC_RSFT, KC_NO, \
+                KC_LGUI, KC_LALT,          KC_BTN1,               KC_RALT,KC_TRNS),
 
 };
 
