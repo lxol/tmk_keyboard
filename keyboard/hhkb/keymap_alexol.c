e59dbf7281e2b6f5ec757094ab4fe7fe0f8aac61
Alex Olkhovskiy <alex@olkhovskiy.com>
2016-01-10 07:00:58 +0000

    wip

--- keyboard/hhkb/keymap_alexol.c
+++ keyboard/hhkb/keymap_alexol.c
@@ -32,7 +32,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     */
     [BASE] = KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
-           LCTL, A,   S,   D,  F,  G,  H,  J,  K,  L,   SCLN,FN3,FN1,             \
+           LCTL, A,   FN8,   D,  F,  G,  H,  J,  K,  FN7,   SCLN, QUOT,FN1,             \
            FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, FN2,FN5,FN0,             \
                 LALT,LGUI,          SPC,                RGUI,RALT),
 
@@ -119,7 +119,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     */
     [CITRIX] = KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
-           LCTL, A,   S,   D,  F,  G,  H,  J,  K,  L,   SCLN,FN3,FN1,             \
+           LCTL, A,   S,   D,  F,  G,  H,  J,  K,  L,   SCLN, QUOT,FN1,             \
            FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, FN6,FN5,FN0,             \
                 RGUI,LGUI,          SPC,                LGUI, RALT),
 
@@ -141,7 +141,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     */
     [WIN] = KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
-           LCTL, A,   S,   D,  F,  G,  H,  J,  K,  L,   SCLN,FN3,FN1,             \
+           LCTL, A,   S,   D,  F,  G,  H,  J,  K,  L,   SCLN,QUOT,FN1,             \
            FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, FN2,FN5,FN0,             \
                 LGUI,LALT,          SPC,                LALT,RGUI),
 
@@ -151,6 +151,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 enum function_id {
     LSHIFT_LPAREN,
     RSHIFT_RPAREN,
+    L_MOD,
 };
 
 
@@ -169,6 +170,8 @@ const uint16_t fn_actions[] PROGMEM = {
     [4] = ACTION_FUNCTION_TAP(LSHIFT_LPAREN),           // Function: LShift with tap '('
     [5] = ACTION_FUNCTION_TAP(RSHIFT_RPAREN),           // Function: RShift with tap ')'
     [6] = ACTION_LAYER_TAP_KEY(VI, KC_SLASH),          // Cursor layer with Slash*
+    [7] = ACTION_FUNCTION_TAP(L_MOD),           //
+    [8] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_S),
     /* [x] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_L),      // RGUI with tap J* */
     /* [x] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_S),      // RGUI with tap F* */
     /* [x] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_A),      // RGUI with tap D* */
@@ -176,71 +179,210 @@ const uint16_t fn_actions[] PROGMEM = {
 
 };
 
+uint16_t l_sentinel_time = 0x00;
+bool l_sentinel = false;
+
+
 
 /*
  * user defined action function
  */
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-    if (record->event.pressed) dprint("P"); else dprint("R");
-    dprintf("%d", record->tap.count);
-    if (record->tap.interrupted) dprint("i");
-    dprint("\n");
+  //debug_record(*record);
+  dprintf("%04X%c(%u)", (record->event.key.row<<8 | record->event.key.col), (record->event.pressed ? 'd' : 'u'), record->event.time);
+  dprintf(":%u%c", record->tap.count, (record->tap.interrupted ? '-' : ' '));
+  dprintf("\n");
+  if (record->event.pressed) dprint("P"); else dprint("R");
+  dprintf("%d", record->tap.count);
+  if (record->tap.interrupted) dprint("i");
+  dprint("\n");
 
-    switch (id) {
-    case LSHIFT_LPAREN:
-      // Shift parentheses example: LShft + tap '('
-      // http://stevelosh.com/blog/2012/10/a-modern-space-cadet/#shift-parentheses
-      // http://geekhack.org/index.php?topic=41989.msg1304899#msg1304899
-      if (record->event.pressed) {
-        if (record->tap.count > 0 && !record->tap.interrupted) {
-          if (record->tap.interrupted) {
-            dprint("tap interrupted\n");
-            register_mods(MOD_BIT(KC_LSHIFT));
-          }
-        } else {
+/*
+|----+----------------+------------------+-----------------+-----------------|
+|    | key1 press(P1) | key1 release(R1) | key2 press (P2) | key2 release R2 |
+|----+----------------+------------------+-----------------+-----------------|
+| P1 | 0              | R1-P1 ()         | P2-P1           | R2-P1           |
+| R1 | -              | 0                | P2-R1 (+,0,-)   | R2-R1           |
+| P2 | -              | -                | 0               | R2-P2           |
+| R2 | -              | -                | -               | 0               |
+|----+----------------+------------------+-----------------+-----------------|
+
+
+| R1-P1      |
+| P2-P1      |
+| R2-P1      |
+| P2-R1      |
+| R2-R1      |
+| R2-P2      |
+| Tap Delays |
+|            |
+
+
+Types of press after tap time:
+| tap | interrupted |     |
+|   1 |           0 | key |
+|   1 |           1 | mod    |
+|   0 |           0 | mod |
+|   0 |           1 |     |
+
+
+after tap time
+- press not interrupteed
+- press tap not interrupted
+- press non tap interrupted
+
+
+| key usage  |              |
+|------------+--------------|
+| normal key |              |
+| modifier   | uninterupted |
+|            |              |
+Delays:
+P1 - R1
+P1 - P2
+
+
+
+|---------------+-----+-------------|
+| press/release | tap | interrupted |
+|---------------+-----+-------------|
+| pressed       |   1 |             |
+| pressed       |   0 |             |
+| relase        |   1 |             |
+| release       |   0 |             |
+|---------------+-----+-------------|
+*/
+  switch (id) {
+  case LSHIFT_LPAREN:
+    // Shift parentheses example: LShft + tap '('
+    // http://stevelosh.com/blog/2012/10/a-modern-space-cadet/#shift-parentheses
+    // http://geekhack.org/index.php?topic=41989.msg1304899#msg1304899
+    if (record->event.pressed) {
+      if (record->tap.count > 0 && !record->tap.interrupted) {
+        dprintf("non interrupted tap");
+        if (record->tap.interrupted) {
+          dprint("tap interrupted\n");
           register_mods(MOD_BIT(KC_LSHIFT));
         }
       } else {
-        if (record->tap.count > 0 && !(record->tap.interrupted)) {
-          add_weak_mods(MOD_BIT(KC_LSHIFT));
-          send_keyboard_report();
-          register_code(KC_9);
-          unregister_code(KC_9);
-          del_weak_mods(MOD_BIT(KC_LSHIFT));
-          send_keyboard_report();
-          record->tap.count = 0;  // ad hoc: cancel tap
-        } else {
-          unregister_mods(MOD_BIT(KC_LSHIFT));
-        }
+        dprintf("press ");
+        register_mods(MOD_BIT(KC_LSHIFT));
       }
-      break;
-    case RSHIFT_RPAREN:
-      // Shift parentheses example: LShft + tap '('
-      // http://stevelosh.com/blog/2012/10/a-modern-space-cadet/#shift-parentheses
-      // http://geekhack.org/index.php?topic=41989.msg1304899#msg1304899
-      if (record->event.pressed) {
-        if (record->tap.count > 0 && !record->tap.interrupted) {
-          if (record->tap.interrupted) {
-            dprint("tap interrupted\n");
-            register_mods(MOD_BIT(KC_RSHIFT));
-          }
+    } else {
+      if (record->tap.count > 0 && !(record->tap.interrupted)) {
+        add_weak_mods(MOD_BIT(KC_LSHIFT));
+        send_keyboard_report();
+        register_code(KC_9);
+        unregister_code(KC_9);
+        del_weak_mods(MOD_BIT(KC_LSHIFT));
+        send_keyboard_report();
+        record->tap.count = 0;  // ad hoc: cancel tap
+      } else {
+        unregister_mods(MOD_BIT(KC_LSHIFT));
+      }
+    }
+    break;
+  case RSHIFT_RPAREN:
+    // Shift parentheses example: LShft + tap '('
+    // http://stevelosh.com/blog/2012/10/a-modern-space-cadet/#shift-parentheses
+    // http://geekhack.org/index.php?topic=41989.msg1304899#msg1304899
+    if (record->event.pressed) {
+      if (record->tap.count > 0 && !record->tap.interrupted) {
+        if (record->tap.interrupted) {
+          dprint("tap interrupted\n");
+          register_mods(MOD_BIT(KC_RSHIFT));
+        }
+      } else {
+        register_mods(MOD_BIT(KC_RSHIFT));
+      }
+    } else {
+      if (record->tap.count > 0 && !(record->tap.interrupted)) {
+        add_weak_mods(MOD_BIT(KC_RSHIFT));
+        send_keyboard_report();
+        register_code(KC_0);
+        unregister_code(KC_0);
+        del_weak_mods(MOD_BIT(KC_RSHIFT));
+        send_keyboard_report();
+        record->tap.count = 0;  // ad hoc: cancel tap
+      } else {
+        unregister_mods(MOD_BIT(KC_RSHIFT));
+      }
+    }
+    break;
+
+  case L_MOD:
+    // Shift parenotheses example: LShft + tap '('
+    // http://stevelosh.com/blog/2012/10/a-modern-space-cadet/#shift-parentheses
+    // http://geekhack.org/index.php?topic=41989.msg1304899#msg1304899
+    if (record->event.pressed) {
+      if (record->tap.count > 0 && !record->tap.interrupted) {
+        l_sentinel_time = record->event.time;
+        register_unregister_kc(KC_L);
+        dprintf("press L tap non interrupted\n");
+      } else if (record->tap.count > 0 && record->tap.interrupted){
+        l_sentinel_time = record->event.time;
+        register_unregister_kc(KC_L);
+        dprintf("press L tap interrupted\n");
+      } else if (!record->tap.count <= 0 && !record->tap.interrupted) {
+        dprintf("press L non tap non interrupted\n");
+        if (record->event.time < (l_sentinel_time + 1000)) {
+            register_code(KC_L);
+            l_sentinel = true;
         } else {
           register_mods(MOD_BIT(KC_RSHIFT));
         }
       } else {
-        if (record->tap.count > 0 && !(record->tap.interrupted)) {
-          add_weak_mods(MOD_BIT(KC_RSHIFT));
-          send_keyboard_report();
-          register_code(KC_0);
-          unregister_code(KC_0);
-          del_weak_mods(MOD_BIT(KC_RSHIFT));
-          send_keyboard_report();
-          record->tap.count = 0;  // ad hoc: cancel tap
+        dprintf("press L non tap interrupted\n");
+        if (record->event.time < (l_sentinel_time + 1000)) {
+            register_code(KC_L);
+            l_sentinel = true;
         } else {
+          register_mods(MOD_BIT(KC_RSHIFT));
+        }
+      }
+    } else {
+      if (record->tap.count > 0 && !record->tap.interrupted) {
+        //register_unregister_kc(KC_L);
+        dprintf("release L tap non interrupted\n");
+        record->tap.count = 0;  // ad hoc: cancel tap
+      } else if (record->tap.count > 0 && record->tap.interrupted){
+        //register_unregister_kc(KC_L);
+        dprintf("release L tap interrupted\n");
+        record->tap.count = 0;  // ad hoc: cancel tap
+      } else if (!record->tap.count <= 0 && !record->tap.interrupted) {
+        dprintf("release L non tap non interrupted\n");
+        if (l_sentinel) {
+          unregister_code(KC_L);
+          l_sentinel = false;
+        }
+        else {
+          unregister_mods(MOD_BIT(KC_RSHIFT));
+        }
+
+      } else {
+        dprintf("release L non tap interrupted\n");
+        if (l_sentinel) {
+          unregister_code(KC_L);
+          l_sentinel = false;
+        }
+        else {
           unregister_mods(MOD_BIT(KC_RSHIFT));
         }
       }
-      break;
     }
+    break;
+  }
 }
+
+void  register_unregister_kc(uint16_t kc) {
+  send_keyboard_report();
+  register_code(kc);
+  unregister_code(kc);
+  send_keyboard_report();
+  //record->tap.count = 0;  // ad hoc: cancel tap
+}
+
+/*
+ * time of the previously pressed l key
+*/
