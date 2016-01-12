59e79c18714e8c372515e699be67d6865706bf05
Alex Olkhovskiy <alex@olkhovskiy.com>
2016-01-12 18:28:24 +0000

    wip

--- keyboard/hhkb_qmk/config.h
+++ keyboard/hhkb_qmk/config.h
@@ -32,17 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 8
 
-// Planck PCB default pin-out
-// Change this to how you wired your keyboard
-// COLS: Left to right, ROWS: Top to bottom
-/* #define COLS (int []){ F1, F0, B0, C7, F4, F5, F6, F7} */
-/* #define ROWS (int []){ D0, D5, B5, B6, D4, D6, B4, D7 } */
-
-/* COL2ROW or ROW2COL */
-//#define DIODE_DIRECTION COL2ROW
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
+#define TAPPING_TERM    200
 
 /* number of backlight levels */
 #define BACKLIGHT_LEVELS 3
@@ -63,7 +53,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
- */
+*/
 
 /* disable debug print */
 //#define NO_DEBUG
